61bf4edf860cb9465d715b51559f44d354242668
Saurabh Rawat <saurabh.rawat90@gmail.com>
2014-03-01 16:50:16 +0530

    Update tests to include more data types.

--- src/test/scala/QuerySpec.scala
+++ src/test/scala/QuerySpec.scala
@@ -8,7 +8,7 @@ import scala.concurrent.duration._
  * Created by eklavya on 18/2/14.
  */
 
-case class Emp(empId: Int, deptId: Int, first: String, last: String)
+case class Emp(empId: Int, deptId: Int, alive: Boolean, id: java.util.UUID, first: String, last: String, salary: Double, age: Long)
 
 class QuerySpec extends FlatSpec with BeforeAndAfterAll with Matchers {
 
@@ -32,23 +32,31 @@ class QuerySpec extends FlatSpec with BeforeAndAfterAll with Matchers {
       """CREATE TABLE emp (
     		empID int,
     		deptID int,
+    		alive boolean,
+    		id uuid,
     		first_name varchar,
     		last_name varchar,
+    		salary double,
+    		age bigint,
+        PRIMARY KEY (empID, deptID))"""/*,
+            age bigint,
+    		salary double,
+    		id uuid,
     		PRIMARY KEY (empID, deptID))
-      """), 5 seconds)
+      """*/), 5 seconds)
     res.isInstanceOf[SchemaChange.type] should be(true)
   }
 
   "Driver" should "be able to execute prepared queries" in {
-    val res = Await.result(Scqla.prepare("INSERT INTO emp (empID, deptID, first_name, last_name) VALUES (?, ?, ?, ?)"), 5 seconds)
-    val res1 = Await.result(res.execute(104, 15, "Hot", "Shot"), 5 seconds)
+    val res = Await.result(Scqla.prepare("INSERT INTO emp (empID, deptID, alive, id, first_name, last_name, salary, age) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"), 5 seconds)
+    val res1 = Await.result(res.execute(104, 15, true, new java.util.UUID(0, 0), "Hot", "Shot", 10000000.0, 98763L), 5 seconds)
     res1.isInstanceOf[Successful.type] should be(true)
   }
 
   "Driver" should "return proper case class list" in {
-    val res = Await.result(Scqla.queryAs[Emp]("select * from emp"), 5 seconds)
+    val res = Await.result(Scqla.queryAs[Emp]("select empID, deptID, alive, id, first_name, last_name, salary, age from emp"), 5 seconds)
     res.foreach(_.isInstanceOf[Emp] should be(true))
-    res.head.asInstanceOf[Emp] should equal(Emp(104, 15, "Hot", "Shot"))
+    res.head.asInstanceOf[Emp] should equal(Emp(104, 15, true, new java.util.UUID(0, 0), "Hot", "Shot", 10000000.0, 98763L))
   }
 
   "Driver" should "return proper primitives" in {
@@ -66,10 +74,10 @@ class QuerySpec extends FlatSpec with BeforeAndAfterAll with Matchers {
   }
 
   "Driver" should "be able to execute prepared queries and get results" in {
-    val res = Await.result(Scqla.prepare("select * from emp where empid = ? and deptid = ?"), 5 seconds)
+    val res = Await.result(Scqla.prepare("select empID, deptID, alive, id, first_name, last_name, salary, age from emp where empid = ? and deptid = ?"), 5 seconds)
     val res1 = Await.result(res.executeGet[Emp](104, 15), 5 seconds)
     res1.foreach(_.isInstanceOf[Emp] should be(true))
-    res1.head.asInstanceOf[Emp] should equal(Emp(104, 15, "Hot", "Shot"))
+    res1.head.asInstanceOf[Emp] should equal(Emp(104, 15, true, new java.util.UUID(0, 0), "Hot", "Shot", 10000000.0, 98763L))
   }
 
   "Driver" should "be able to drop keyspace" in {
